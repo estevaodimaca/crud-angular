9b1b9fa3d1f51aa4dc9403438de62bce92f659df
Estevao Dimaca <estevaodimaca@gmail.com>
2024-12-26 15:11:44 +0200

    04 - Criando o Módulo de Cursos e Usando Roteamento com Lazy Loading

--- src/app/courses/courses/courses.component.ts
+++ src/app/courses/courses/courses.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { Course } from '../model/course';
 
 @Component({
   selector: 'app-courses',
-  imports: [],
+  imports: [MatTableModule],
   templateUrl: './courses.component.html',
   styleUrl: './courses.component.scss'
 })
-export class CoursesComponent {
+export class CoursesComponent implements OnInit{
 
+  courses: Course[] = [
+    {id: "1", name: "Angular", category: "frontend"}
+  ];
+  displayedColumns: string[] = ['id', 'name', 'category'];
+
+  ngOnInit(): void {
+
+  }
 }
